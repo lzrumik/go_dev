0066c0dbb2b2de79be76dec1e7dbfab2a545ae79
ran.li <ran.li@fengjr.com>
2019-07-31 12:13:24 +0800

    添加string修改

---
+++ src/ch02_string/02string_func/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	base_demo();
+
+	string_to_byte()
+
+	fmt.Println(comma("12345"))
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func string_to_byte(){
+
+	s := "abc"
+	b := []byte(s)
+	s2 := string(b)
+
+	fmt.Println(s,s2)
+}
+
+func base_demo(){
+
+	fmt.Println(basename("a/b/c.go")) // "c"
+	fmt.Println(basename("c.d.go"))   // "c.d"
+	fmt.Println(basename("abc"))      // "abc"
+
+
+	fmt.Println(basename2("a/b/c.go")) // "c"
+	fmt.Println(basename2("c.d.go"))   // "c.d"
+	fmt.Println(basename2("abc"))      // "abc"
+}
+
+func basename(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// Preserve everything before last '.'.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func basename2(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
