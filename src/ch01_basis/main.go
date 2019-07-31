b002220b37b528460dabb679808ad8e8fd2e2270
ran.li <ran.li@fengjr.com>
2019-07-31 18:07:56 +0800

    调整123

--- src/ch01_basis/main.go
+++ src/ch01_basis/main.go
@@ -1,42 +1,23 @@
 package main
 
-import "fmt"
-
-type Point struct { X, Y int }
-
-type Rect1 struct { Min, Max Point }
-type Rect2 struct { Min, Max *Point }
-
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 func main() {
+	seen := make(map[string]bool) // a set of strings
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
 
-	p := Point{10,20}
-	pp := &Point{10,20}
-
-	fmt.Println(pp,p)
-	fmt.Printf("%p %d\n",pp,p)
-
-	r1 := Rect1{Point{10,20},Point{50,60}}
-	r2 := Rect2{&Point{10,20},&Point{50,60}}
-
-	fmt.Println(r1,r2)
-
-
-	fmt.Println("=================================")
-
-	tSlice := []int{1,2,3,4,5}
-	fmt.Println(tSlice)
-
-
-
-
-
-	var r *Rect = &Rect{10,20}
-	fmt.Println(r)
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+		os.Exit(1)
+	}
 }
-
-
-type Rect struct {
-	width float64
-	height float64
-}
-
