73eab37e66123789df80cc83575e624fb862102c
ran.li <ran.li@fengjr.com>
2019-08-07 16:38:50 +0800

    删除无用代码

--- src/ch84_beego_api_project/models/fortune_role_node.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type FortuneRoleNode struct {
-	RoleId      string    `orm:"column(role_id);size(36)" description:"角色ID"`
-	NodeId      string    `orm:"column(node_id);size(36);null" description:"节点ID"`
-	DateEntered time.Time `orm:"column(date_entered);type(datetime);null" description:"创建时间"`
-}
